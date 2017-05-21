defb4a76fde4bd9a2908ad6d96964ab05ce2eb55
Petr Karmashev <smonkl@bk.ru>
2017-05-22 00:03:18 +0300

    Login component added

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Authenticator } from '../../services/Authenticator';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+    model: any = {};
+    loading = false;
+    returnUrl: string;
+
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticator: Authenticator
+    ) {
+    }
+
+    ngOnInit() {
+        this.authenticator.logout();
+
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+
+    login() {
+        this.loading = true;
+        this.authenticator.login(this.model.username, this.model.password)
+            .subscribe(
+                data => {
+                    this.router.navigate([this.returnUrl]);
+                },
+                error => {
+                    console.log(error);
+                    this.loading = false;
+                }
+            );
+    }
+}
