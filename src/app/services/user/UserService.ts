f491bb866851bc1238fe77a1261d59e84ff564ec
Petr Karmashev <smonkl@bk.ru>
2017-06-09 00:59:28 +0300

    UserService added

---
+++ src/app/services/user/UserService.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '../http/HttpClient';
+import { environment } from '../../../environments/environment';
+
+@Injectable()
+export class UserService {
+    private userName: string;
+    private flatNumber: number;
+
+    constructor(private httpClient: HttpClient) {
+    }
+
+    public getUserName(): string {
+        return this.userName;
+    }
+
+    public setUserName(userName: string) {
+        this.userName = userName;
+    }
+
+    public getFlatNumber(): number {
+        return this.flatNumber;
+    }
+
+    public setFlatNumber(flatNumber: number) {
+        this.flatNumber = flatNumber;
+    }
+
+    public requestUser(): void {
+        this.httpClient.get(environment.apiUrl + '/api/user')
+            .subscribe(result => {
+                const responseData = result.json();
+                this.setUserName(responseData.name);
+                this.setFlatNumber(responseData.name);
+            });
+    }
+}
